d9656b065b71df8ebf1584e7475c8bdbb1b1969e
정문경 <developer.frontdoor@gmail.com>
2022-02-09 10:00:00 +0900

    Program.cs

--- Code/Program.cs
+++ Code/Program.cs
@@ -2,26 +2,6 @@
 
 class Program
 {
-    public static double GetMagicNumber()
-    {
-         var random = new Random();
-         return random.NextDouble();
-    }
-    public static int GetRandomInt()
-    {
-        var random = new Random();
-        return random.Next(1,10);
-    }
-
-    public static int GetRandom()
-    {
-<<<<<<< HEAD
-        var random = new Random();
-        return random.Next(1,10);
-=======
-        Console.WriteLine("Hello, World!");
->>>>>>> parent of 7042f98... Program.cs
-    }
 
     static void Main(string[] args)
     {
